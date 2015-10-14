cf64f4e04717a43b48226feac0151d8a2815af61
Adam Lopez <Adamf04@gmail.com>
2015-10-14 17:11:24 -0400

    Added information to shake

--- shake.rb
+++ shake.rb
@@ -1,16 +1,34 @@
 class MilkShake
 	def initialize
 		@base_price = 3
-		@ingredients = [ ]
+		@ingredients = [ "Vanilla" , "Coconut" , "Pistachio" ]
 	end
 
 	def add_ingredient(ingredient)
 		@ingredients.push(ingredient)
 	end	
+
+	def price_of_milkshake
+		@total_price_of_milkshake = @base_price
+		@ingredients.each do |ingredient|
+			@total_price_of_milkshake += ingredient.price
+		end
+		@total_price_of_milkshake
+	end
+end
+
+
+class Ingredient
+	def initialize(name, price)
+		@name = name
+		@price = price
+	end
 end
 
 nizars_milkshake = MilkShake.new
 banana = Ingredient.new("Banana", 2)
-chocolate_chips = Ingredient.new("Chocolate chips", 1)
-nizars_milkshake.add_ingredient(banana)
-nizars_milkshake.add_ingredient(chocolate_chips)
+chocolate_chips = Ingredient.new("Chocolate Chips")
+
+puts nizars_milkshake
+
+
